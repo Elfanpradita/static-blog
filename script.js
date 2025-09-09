e9269af73a2421a6a4c61c7c2d00e046f3f70da2
Elfan Pradita Rusmin <elfanpradita@Elfans-MacBook-Air.local>
2025-09-09 11:12:38 +0700

    fresh banget

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("toggleTheme");
-  toggleBtn.addEvent
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+      toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+      toggleBtn.textContent = "🌙 Dark Mode";
+    }
+  });
+});
